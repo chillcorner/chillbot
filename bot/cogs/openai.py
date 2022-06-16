2a697ae8b0d7467cb9a6e9f53a2bf577c098ab8d
bharat <admin@bharat.com>
2022-06-16 07:54:42 +0530

    refactored openai cog

--- bot/cogs/openai.py
+++ bot/cogs/openai.py
@@ -6,17 +6,29 @@ from bot.constants import Keys
 
 openai.api_key = Keys.openai_key
 
+EMOJI_RE = re.compile(r'<(a?):([A-Za-z0-9_]+):([0-9]+)>')
+MENTION_RE = re.compile(r'<@?(!?)(#?)(&?)([0-9]*)>')
+
 
 def clean_message(msg):
     """Removes emojis, mentions, and links from a message."""
 
-    emoji_pattern = re.compile(r'<(a?):([A-Za-z0-9_]+):([0-9]+)>')
-    mentions_pattern = re.compile(r'<@?(!?)(#?)(&?)([0-9]*)>')
+    msg = re.sub(MENTION_RE, '', msg)
+    new_msg = re.sub(EMOJI_RE, '', msg)
 
-    text = re.sub(emoji_pattern, '', msg)
-    text = re.sub(mentions_pattern, '', text)
+    return new_msg.strip()
 
-    return text.strip()
+
+def is_clean_with_ref(ctx):
+    """Checks if the message is clean and has a reference."""
+    ref = ctx.message.reference
+    if not ref:
+        return
+
+    clean = clean_message(ref.cached_message.content)
+
+    if not clean:
+        return
 
 
 class OpenAI(commands.Cog):
@@ -46,45 +58,35 @@ class OpenAI(commands.Cog):
     @commands.cooldown(1, 30, commands.BucketType.member)
     async def explain(self, ctx):
         """Explain a text for you"""
-        ref = ctx.message.reference
-        if not ref:
-            return
-
-        clean = clean_message(ref.cached_message.content)
-
-        if not clean:
+        if not is_clean_with_ref(ctx):
             return
 
         res = self.get_openapi_response(prompt=f"Explain the meaning of this text from {ref.cached_message.author.display_name.title()}:\n{clean}\nExplanation:",
-                                              stop="Explanation:", tokens=256)
+                                        stop="Explanation:", tokens=256)
         await ctx.send(content=res, reference=ref.cached_message.to_reference())
 
     @commands.command()
     @commands.cooldown(1, 30, commands.BucketType.member)
     async def english(self, ctx):
         """Corrects grammar and spelling errors in a text for you"""
-        ref = ctx.message.reference
-        if not ref:
-            return
-
-        clean = clean_message(ref.cached_message.content)
-
-        if not clean:
+        if not is_clean_with_ref(ctx):
             return
 
         res = self.get_openapi_response(prompt=f"Correct this to standard English:\n{clean}\ncorrection:",
-                                              stop="Correction:", tokens=60)
+                                        stop="Correction:", tokens=60)
         await ctx.send(content=res, reference=ref.cached_message.to_reference())
 
     @commands.command()
     @commands.cooldown(1, 30, commands.BucketType.member)
     async def fact(self, ctx, *, topic=None):
         """Tells you a random fact about given topic."""
+        if topic:
+            prompt = clean_message(topic)        
 
         prompt = "Tell me a random fact" if not topic else f"Tell me a fact about {topic}."
 
         res = self.get_openapi_response(prompt=f"{prompt}\nfact:",
-                                              stop="Fact:", tokens=60)
+                                        stop="Fact:", tokens=60)
         await ctx.send(content=res)
 
 
