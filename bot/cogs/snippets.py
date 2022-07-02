9bdbe73a7056a1e0a974577f77546f93e9c17598
Bharat <60933235+bharatbots@users.noreply.github.com>
2022-07-02 11:02:42 +0530

    snippet add command added

--- bot/cogs/snippets.py
+++ bot/cogs/snippets.py
@@ -2,6 +2,8 @@ import re
 import discord
 from discord.ext import commands
 
+from bot.constants import Channels
+
 IMAGE_URL_PATTERN = re.compile(
     r'(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|jpeg|gif|png|svg)', re.IGNORECASE)
 
@@ -35,7 +37,7 @@ class Snippets(commands.Cog):
 
         row = await self.bot.pool.fetchrow("""SELECT * FROM snippets WHERE name = $1""", cmd)
         if not row:
-            return ## await msg.channel.send(f"Found nothing.")
+            return  # await msg.channel.send(f"Found nothing.")
 
         # check for cooldown
         on_cooldown = await self.is_on_snippet_cooldown(msg)
@@ -69,6 +71,41 @@ class Snippets(commands.Cog):
 
         # send snippet
         tmp_msg = await msg.channel.send(reference=msg, embed=embed)
+
+    @commands.group(name="snippet", invoke_without_command=False)
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def snippet(self, ctx):
+        """Snippet commands"""
+        pass
+
+    @snippet.command(name="add")
+    @commands.is_owner()
+    async def snippet_add(self, ctx, *, unique_name: str):
+        """Add a snippet"""
+        ref = ctx.message.reference
+        if not ref:
+            return
+        if not ref.attachments:
+            return await ctx.send("No image attached.")
+
+        url = ref.attachments[0].url
+        if re.match(IMAGE_URL_PATTERN, url):
+            storage_channel = self.bot.get_channel(Channels.storage)
+            msg = await storage_channel.send(url)
+
+            if msg.attachments:
+                url = msg.attachments[0].url
+            else:
+                return await ctx.send(f"Couldn't upload the file.")
+
+            try:
+                await self.bot.pool.execute("""INSERT INTO snippets(name, approved, title, description, footer, owner_id, storage_id)
+                                        VALUES($1, $2, $3, $4, $5, $6, $7)""",
+                                            unique_name, True, None, url, None, ctx.author.id, msg.id)
+            except Exception as e:
+                return await ctx.send(f"I couldn't add the snippet: {e}")
+
+            await ctx.send(f"Snippet {unique_name} added.")
 
 
 async def setup(bot):
