0a6fa3bfab4d5bd46f72828e883b7f3c7ffd798c
bharat <60933235+bharatbots@users.noreply.github.com>
2023-08-15 18:02:51 +0530

    fun cmd

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -1,5 +1,7 @@
 import re
 from typing import Optional
+import random
+import asyncio 
 
 import discord
 from discord.ext import commands
@@ -24,11 +26,65 @@ class Fun(commands.Cog):
                     await after.send(f"You aren't allowed to change your nickname.")
                 except Exception as e:
                     raise e
-                
+
                 try:
                     await after.edit(nick=fixed_nick)
                 except Exception as e:
                     raise e
+
+    @commands.command()
+    @commands.is_owner()
+    async def sn(self, ctx, minutes: int):
+        if not ctx.guild:
+            await ctx.send("This command can only be used in a server!")
+            return
+
+        # Ensure the user has permission to change nicknames
+        if not ctx.author.guild_permissions.manage_nicknames:
+            await ctx.send("You do not have permission to change nicknames!")
+            return
+
+        # Fetch the last 25 messages
+        messages = await ctx.channel.history(limit=25).flatten()
+
+        # Get unique authors (excluding the bot)
+        authors = list(
+            {message.author for message in messages if not message.author.bot}
+        )
+
+        if len(authors) < 2:  # We need at least two members to shuffle
+            await ctx.send("Not enough unique members to shuffle!")
+            return
+
+        # Backup their nicknames and shuffle
+        original_nicks = {}
+        for author in authors:
+            original_nicks[author.id] = author.nick
+
+        shuffled_nicks = list(original_nicks.values())
+        random.shuffle(shuffled_nicks)
+
+        for author, new_nick in zip(authors, shuffled_nicks):
+            try:
+                await author.edit(nick=new_nick)
+            except discord.Forbidden:
+                print(
+                    f"Couldn't change the nickname for {author.name} due to missing permissions."
+                )
+            except discord.HTTPException as e:
+                print(f"Error while changing the nickname for {author.name}: {e}")
+
+        # Wait for the specified time, then revert the nicknames
+        await asyncio.sleep(minutes * 60)
+
+        for author in authors:
+            try:
+                await author.edit(nick=original_nicks[author.id])
+            except discord.Forbidden:
+                pass  # Silently pass if there's a permission issue upon reverting
+            except discord.HTTPException:
+                pass
+
 
 async def setup(bot):
     await bot.add_cog(Fun(bot))
