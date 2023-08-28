315b3bbe20c7515758dbeb1ffb5140188a087dff
bharat <60933235+bharatbots@users.noreply.github.com>
2023-08-28 10:55:12 +0530

    fun hug struggle

--- bot/cogs/hug_struggle.py
+++ bot/cogs/hug_struggle.py
@@ -52,7 +52,9 @@ class LockChannelCog(commands.Cog):
             if reactions:
                 if reactions[0].count > self.target_count:
                     await channel.set_permissions(
-                        channel.guild.default_role, send_messages=True
+                        channel.guild.default_role, send_messages=True,
+                        embed_links=False,
+                        attach_files=False,
                     )
                     await channel.send(f"Channel has been unlocked!")
                     del self.locked_channels[payload.channel_id]
