25fb8f53ec17fd4b025a6e6fa8f8e218abd6f690
bharat <60933235+bharatbots@users.noreply.github.com>
2023-08-30 08:15:36 +0530

    test cat

--- bot/cogs/fun.py
+++ bot/cogs/fun.py
@@ -3,91 +3,61 @@ from typing import Optional
 import random
 import asyncio
 import time
+from io import BytesIO
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 from discord.ext.commands import BucketType, CommandOnCooldown, CooldownMapping
 
-from bot.cogs.utils import time
 from bot.constants import Channels, Roles, Whitelists
-
-fixed_nicks = {999177926903869520: "killjoy", 982097011434201108: "bharat"}
 
 
 class Fun(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.last_kill
+        self.change_role_color.start()
 
     @commands.Cog.listener()
-    async def on_member_update(self, before, after):
-        if before.nick == after.nick:
-            return
-        if after.id in fixed_nicks:
-            fixed_nick = fixed_nicks.get(after.id)
-            if not after.display_name.lower().startswith(fixed_nick.lower()):
-                try:
-                    await after.send(f"You aren't allowed to change your nickname.")
-                except Exception as e:
-                    raise e
-
-                try:
-                    await after.edit(nick=fixed_nick)
-                except Exception as e:
-                    raise e
-
-    @commands.command()
-    @commands.is_owner()
-    async def sn(self, ctx, minutes: int):
-        if not ctx.guild:
-            await ctx.send("This command can only be used in a server!")
+    async def on_message(self, msg):
+        if msg.author.bot:
             return
 
-        # Ensure the user has permission to change nicknames
-        if not ctx.author.guild_permissions.manage_nicknames:
-            await ctx.send("You do not have permission to change nicknames!")
+        if not msg.guild:
             return
 
-        # Fetch the last 25 messages
-        messages = [msg async for msg in ctx.channel.history(limit=25)]
-
-        # Get unique authors (excluding the bot)
-        authors = list(
-            {message.author for message in messages if not message.author.bot}
-        )
-
-        if len(authors) < 2:  # We need at least two members to shuffle
-            await ctx.send("Not enough unique members to shuffle!")
+        role = msg.guild.get_role(1146271394280255498)
+        if role in msg.author.roles:
             return
 
-        # Backup their nicknames and shuffle
-        original_nicks = {}
-        for author in authors:
-            original_nicks[author.id] = author.nick
+        try:
+            await msg.author.add_roles(role)
+            cat_emoji = self.bot.get_emoji(564512595445284875)
+            await msg.add_reaction(cat_emoji)
+        except discord.HTTPException as e:
+            print(e)
 
-        shuffled_nicks = list(original_nicks.values())
-        random.shuffle(shuffled_nicks)
+    # every 2 mins change role color
+    @tasks.loop(minutes=1)
+    async def change_role_color(self):
+        guild = self.bot.get_guild(444470893599784960)
+        role = guild.get_role(1146271394280255498)
+        colors = [
+            discord.Color.red(),
+            discord.Color.orange(),
+            discord.Color.gold(),
+            discord.Color.green(),
+            discord.Color.blue(),
+            discord.Color.purple(),
+        ]
 
-        for author, new_nick in zip(authors, shuffled_nicks):
-            try:
-                await author.edit(nick=new_nick)
-            except discord.Forbidden:
-                print(
-                    f"Couldn't change the nickname for {author.name} due to missing permissions."
-                )
-            except discord.HTTPException as e:
-                print(f"Error while changing the nickname for {author.name}: {e}")
+        # change role image to random cat emoji image
+        cat_emojis = [e for e in self.bot.emojis if "cat" in e.name]
 
-        # Wait for the specified time, then revert the nicknames
-        await asyncio.sleep(minutes * 60)
+        cat_emoji = random.choice(cat_emojis)
 
-        for author in authors:
-            try:
-                await author.edit(nick=original_nicks[author.id])
-            except discord.Forbidden:
-                pass  # Silently pass if there's a permission issue upon reverting
-            except discord.HTTPException:
-                pass
+        img_bytes = await cat_emoji.url.read()
+
+        await role.edit(color=random.choice(colors), display_icon=img_bytes)
 
 
 async def setup(bot):
